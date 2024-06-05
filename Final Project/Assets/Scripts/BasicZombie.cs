9cdb1975b3c95fbdf8be758b4f00db451c2f6f68
jcgarcias2016 <jc.garcias.2016@alumnos.urjc.es>
2024-06-05 12:12:24 +0200

    Starting to add more complex enemy, added attack animation and started to migrate functionality from the previous enemy.

---
+++ Final Project/Assets/Scripts/BasicZombie.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicZombie : Enemy
+{
+    private Vector3 movement = Vector3.zero;
+    private Vector3 direction = Vector3.zero;
+    private bool moving = false;
+    private float timeBetweenAttacks = 1f;
+    private float timePastAttack = 0f;
+    [SerializeField] private int damage;
+
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 2.5f)
+        {
+            moving = true;
+            Movement();
+        }
+        else if (moving)
+        {
+            moving = false;
+            animator.SetBool("Moving", false);
+        }
+
+    }
+
+    public override void Movement()
+    {
+        base.Movement();
+        direction = player.transform.position - transform.position;
+        Vector3.Normalize(direction);
+        if (Vector3.Distance(transform.position, player.transform.position) < 0.5f)
+        {
+            timePastAttack += Time.deltaTime;
+            if (timePastAttack > timeBetweenAttacks)
+            {
+                player.GetComponent<PlayerHealth>().TakeDamage(damage);
+                timePastAttack = 0f;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            if (timePastAttack != 0f)
+            {
+                timePastAttack = 0f;
+            }
+        }
+        //direction = movement.normalized;
+        //enemyRigidBody.MovePosition(movement);
+        if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+        animator.SetFloat("MoveX", direction.x);
+        animator.SetFloat("MoveY", direction.y);
+        animator.SetBool("Moving", true);
+    }
+}
